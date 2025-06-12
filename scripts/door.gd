5c96a561f837d13b4565acbc200c978c0785d797
NicolasRV1 <nicolasramos105341@hotmail.com>
2025-06-12 01:36:28 -0300

    hut interior

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,13 +1,24 @@
 extends Area2D
 
 var canEnter = false
+var inside = false
 
 func _on_action_point_entered(area: Area2D) -> void:
 	canEnter = true
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("action") and canEnter:
+	
+	
+	if Input.is_action_just_pressed("action") and canEnter and !inside:
+		
 		get_tree().change_scene_to_file("res://fishFishfish/scenes/insideHut.tscn")
+		inside = true
+		
+		if inside:
+			print("saiii")
+
+
+
 
 
 func _on_area_exited(area: Area2D) -> void:

9f65251d4a3e0895e126fdf683b12e41e18df432
NicolasRV1 <nicolasramos105341@hotmail.com>
2025-06-12 18:46:55 -0300

    a try to fix transictions between scenes
    none of the new scripts worked, but it can be worth it in another way

--- scripts/door.gd
+++ scripts/door.gd
@@ -3,20 +3,21 @@ extends Area2D
 var canEnter = false
 var inside = false
 
+
 func _on_action_point_entered(area: Area2D) -> void:
 	canEnter = true
 
 func _process(delta: float) -> void:
 	
 	
-	if Input.is_action_just_pressed("action") and canEnter and !inside:
-		
-		get_tree().change_scene_to_file("res://fishFishfish/scenes/insideHut.tscn")
-		inside = true
-		
+	if Input.is_action_just_pressed("action") and canEnter:
+		if !inside:
+			get_tree().change_scene_to_file("res://fishFishfish/scenes/insideHut.tscn")
+			
+			inside = true
+			
 		if inside:
 			print("saiii")
-
 
 
 
